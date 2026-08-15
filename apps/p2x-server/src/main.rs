13e6742fdf6b4444f7ee621dc02a0bbbca2d6f17
Bruce Tsai <bruce.tsai@opennet.tw>
2026-08-15 02:36:56 +0000

    Include destination peer in relay listener request

--- apps/p2x-server/src/main.rs
+++ apps/p2x-server/src/main.rs
@@ -43,7 +43,9 @@ async fn main() -> io::Result<()> {
                 )
             })?;
         swarm.dial(exchange.clone()).map_err(io::Error::other)?;
-        let circuit = exchange.with(Protocol::P2pCircuit);
+        let circuit = exchange
+            .with(Protocol::P2pCircuit)
+            .with(Protocol::P2p(*swarm.local_peer_id()));
         relay_peer_id = Some(relay_peer);
         pending_circuit = Some(circuit.clone());
         let advertised = circuit.clone().with(Protocol::P2p(*swarm.local_peer_id()));
